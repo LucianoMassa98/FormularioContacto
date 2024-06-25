06c374bdefbdee90f9772fdc2bc8ae6754e87de6
lucianomassa98 <lgmassa98@gmail.com>
2024-06-25 20:38:00 -0300

    fixed rta enviarEmail #4

--- services/formularios.service.js
+++ services/formularios.service.js
@@ -7,13 +7,19 @@ const { EnviarEmail, enviarCorreoExitoso } = require("../modules/EnviarEmail");
 class FormulariosService {
   async create(data) {
 
+    const mensaje = await this.prettyPrintJSON(JSON.stringify(data, null, 2));
     // Enviar correo electrónico de verificación
     const rta = await EnviarEmail(
       config.emailReceptor,
-      "Formulario Suinfi",
-      JSON.stringify(data, null, 2)
+      "Nuevo Formulario Suinfi De Contacto",
+      mensaje
     );
     return rta;
   }
+
+  async prettyPrintJSON(jsonString) {
+    const lines = jsonString.split('\n');
+    return lines.map(line => '    ' + line).join('\n');
+}
 }
 module.exports = FormulariosService;
